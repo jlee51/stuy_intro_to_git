b0d6517928b99005963c550e49bf9c635e1ef3b0
Joseph Lee <jlee51@stuy.edu>
2018-05-31 10:03:34 -0400

    fixed bugggggs

--- Sample_Shell_Script.sh
+++ Sample_Shell_Script.sh
@@ -1,14 +1,18 @@
+
 #!/usr/bin/env bash
 
 echo "Total number of words:"
 cat ~/dictionary.txt | wc --words
 echo ""
 
-# To fix this, you need to filter the dictionary words
+# To fix this, you need to filter the dictionary words right?
 echo "Number of words containing the string 'auto':"
-cat ~/dictionary.txt | FILL_ME_IN | wc --words
+cat ~/dictionary.txt | grep auto | wc --words
 echo ""
 
 # For this next bug, you may have to research options for the `sort` command using `$ man sort`
 echo "Alphabetically reverse-sorted list of words containing the string 'auto':"
-cat ~/dictionary.txt | grep auto | sort
+cat ~/dictionary.txt | grep auto | sort -r
+
+git commit-am "Added grep auto to find the number of words with the word auto"
+git commit-am "used sort -r to reverse the order of the list"
